0632125835d2758c31e763abea2a06ea4d005132
Carlos Henrique <ichfs9108@gmail.com>
2023-09-16 00:50:05 -0300

    Project created

---
+++ event-city/src/test/java/com/tddeventcity/eventcity/EventCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tddeventcity.eventcity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
