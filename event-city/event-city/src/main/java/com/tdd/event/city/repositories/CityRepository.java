d88d6f6e84b38ac9c9aababc0ade571598c554d5
Carlos Henrique <ichfs9108@gmail.com>
2023-09-17 14:34:48 -0300

    Add - Repositories

---
+++ event-city/event-city/src/main/java/com/tdd/event/city/repositories/CityRepository.java
@@ -0,0 +1,9 @@
+package com.tdd.event.city.repositories;
+
+import com.tdd.event.city.entities.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Long> {
+}
